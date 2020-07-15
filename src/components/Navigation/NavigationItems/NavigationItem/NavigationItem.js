d95df0a1fca2d5b9c1002b68f0973855945fa0b2
lerderl <josepholukunle1107@gmail.com>
2020-07-15 10:49:16 +0100

    Added responsive side drawer

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './NavigationItem.module.css';
 
 const navigationItem = (props) => (
-    <li className={classes.NavigstionItem}><a href={props.link} className={props.active ? classes.active : null}>
+    <li className={classes.NavigationItem}><a href={props.link} className={props.active ? classes.active : null}>
         {props.children}</a>
     </li>
 );
