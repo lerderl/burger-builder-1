cad9119125e5af95c78b452a6392bb0839800090
lerderl <josepholukunle1107@gmail.com>
2020-07-18 11:52:00 +0100

    Set-up navigation to the checkoutpage

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import classes from './Burger.module.css';
 import BurgerIngredient from './Burgeringredient/Burgeringredient';
 
 const burger = (props) => {
+    console.log(props);
     let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />;
@@ -28,4 +30,4 @@ const burger = (props) => {
     );
 };
 
-export default burger;
+export default withRouter(burger);
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,6 +31,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
+        console.log(this.props);
         axios.get('https://my-react-burger-3d4d6.firebaseio.com/ingredients.json').then(response => {
             this.setState({ingredients: response.data});
         }).catch(error => {
@@ -92,30 +93,31 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('Proceed!');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Praise Funmi',
-                address: {
-                    street: 'Teststreet',
-                    zipCode: '23401',
-                    country: 'nigeria'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
+        // // alert('Proceed!');
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Praise Funmi',
+        //         address: {
+        //             street: 'Teststreet',
+        //             zipCode: '23401',
+        //             country: 'nigeria'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false});
+        //     });
+        this.props.history.push('/checkout');
     }
     
     render() {
